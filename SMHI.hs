60837f188d77057899debb2fbee348f6a2ddb14e
Herbert Lange <sonofchaos@gmx.de>
2016-11-08 17:04:09 +0100

    move the getData into plotData

--- SMHI.hs
+++ SMHI.hs
@@ -10,17 +10,6 @@ import Haste.Graphics.Canvas
 -- for all stations
 dataUrl = "http://opendata-download-metobs.smhi.se/api/version/1.0/parameter/1/station/71420/period/latest-months/data.json"
 
-getData :: IO JSON
-getData =
-  do
-    writeLog dataUrl
-    let req = getRequest dataUrl
-    rsp <- simpleHTTP req
-    writeLog "foobar"
---    body <- getResponseBody rsp
---    let decode = decodeJSON $ toJSString body
---    return $ either (\_ -> Null) id decode
-    return Null
 
 pointsToShape :: [Point] -> Shape ()
 pointsToShape [] =
@@ -39,13 +28,38 @@ dataToPoints pos (j:js) =
   in
     (pos,y):(dataToPoints (pos + 1) js)
 
-plotData :: Canvas -> JSON -> IO ()
-plotData canvas dat =
+takeFromEnd :: Int -> [a] -> [a]
+takeFromEnd num l =
+  reverse $ take num $ reverse l
+  
+plotData :: Canvas -> IO ()
+plotData canvas =
+  let callback :: Maybe JSON -> IO ()
+      callback (Just dat) =
+        do
+          let Arr dataPoints = ( dat ! (toJSString "value"))
+          let points = dataToPoints 0 $ takeFromEnd 800 $ dataPoints
+          let picture = fill $ pointsToShape points
+          render canvas $ do
+            setFillColor (RGB 128 128 128)
+            picture
+            setStrokeColor (RGB 0 0 0)
+            stroke $ do
+              line (0,250) (800,250)
+              line (0,200) (800,200)
+              line (0,150) (800,150)
+              line (0,100) (800,100)
+              line (0, 50) (800, 50)
+            setFillColor (RGB 0 0 0)
+            text (10,290) "-20° C"
+            text (10,240) "-10° C"
+            text (10,190) "0° C"
+            text (10,140) "10° C"
+            text (10, 90) "20° C"
+            text (10, 40) "30° C"
+      callback Nothing = return ()
+  in
   do
-    let Arr dataPoints = ( dat ! (toJSString "value"))
-    let points = dataToPoints 0 dataPoints
-    let picture = fill $ pointsToShape points 
-    render canvas $ do
-      setFillColor (RGB 128 128 128)
-      picture
+    writeLog dataUrl
+    ajaxRequest GET dataUrl noParams callback
     
