68d1f001dae5a5d5d9176e7402f03f5271068d1e
Herbert Lange <sonofchaos@gmx.de>
2016-11-08 15:50:47 +0100

    remove module header and implement a basic main function

--- Main.hs
+++ Main.hs
@@ -1,7 +1,13 @@
-module Main where
 import SMHI
 import Haste
 import Haste.Graphics.Canvas
 
 main :: IO ()
-main = return ()
+main =
+  do
+    writeLog "Starting"
+    Just canvas <- getCanvasById "canvas"
+    dat <- getData
+    writeLog $ show dat
+    plotData canvas dat
+    return ()
